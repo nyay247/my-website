25f1b966bbd77f339f59ccd268af611916c4f633
nyay247 <nyay247@gmail.com>
2026-04-17 18:20:34 +0530

    Delete svgs directory

--- svgs/index.js
+++
@@ -1,4 +0,0 @@
-export { ReactComponent as Linkdin } from "./linkedin.svg";
-export { ReactComponent as Instagram } from "./instagram.svg";
-export { ReactComponent as Twitter } from "./twitter.svg";
-export { ReactComponent as Youtube } from "./youtube.svg";
